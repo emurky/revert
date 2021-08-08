db3610110a9ab7c967f609b56dfda12be834c92f
Safar Safarly <hafl@me.com>
2021-08-08 16:01:23 +0300

    new try

--- revert.c
+++ revert.c
@@ -35,51 +35,5 @@ int		check_arguments(int argc, char *file_path)
 
 void	print_reverted_lines(int fd)
 {
-	ssize_t		linelen = 0;
-	int 		file_pos = 0;
-	int			read_check;
-	char		c = -1;
-
-	lseek(fd, -2, SEEK_END);
-	while (file_pos != -1) {
-		read_check = read(fd, &c, sizeof(char));
-		while (c != '\n') {
-			file_pos = lseek(fd, -2, SEEK_CUR);
-			if (file_pos == -1) {
-				lseek(fd, -1, SEEK_CUR);
-				printline(fd);
-				return ;
-			}
-			read_check = read(fd, &c, sizeof(char));
-		}
-		linelen = printline(fd);
-		file_pos = lseek(fd, -(linelen + 2), SEEK_CUR);
-	}
-	write(1, "\n", 1);
-
-	return ;
-}
-
-size_t	printline(int fd)
-{
-	char		c = -1;
-	size_t		linelen = 1;
-	int			read_check;
-
-	read_check = read(fd, &c, sizeof(char));
-	if (c == EOF) {
-		return (1);
-	}
-	while (c != '\n' && read_check) {
-		write(1, &c, 1);
-		read_check = read(fd, &c, sizeof(char));
-		linelen++;
-	}
-	/* it is commented to be more alike tac */
-	// if (!read_check) {
-	// 	linelen--;
-	// }
-	write(1, "\n", 1);
-
-	return (linelen);
+	(void)fd;
 }
