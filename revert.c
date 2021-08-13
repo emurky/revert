c5cea68f1507762865ea353ec94f2d08cdde222d
Safar Safarly <hafl@me.com>
2021-08-13 03:46:14 +0300

    saving changes

--- revert.c
+++ revert.c
@@ -44,17 +44,11 @@ void	print_reverted_lines(int fd)
 	int		nl_pos = 0;
 	int		linelen = 0;
 	int		lseek_ret = 0;
-	int		remainder_len = 0;
-	char	remainder[BUFFER_SIZE + 1];
 	int		buffers_without_nl = 0;
+int	buffers_was_printed = false;
 
 lseek_ret = lseek(fd, -(1 + BUFFER_SIZE), SEEK_END);
-	// read_ret = read(fd, buffer, BUFFER_SIZE);
-	// buffer[read_ret] = '\0';
-	// lseek_ret = lseek(fd, -1, SEEK_END);
-	// nl_pointer = strrchr(buffer, '\n');
 	do {
-		here:
 		read_ret = read(fd, buffer, BUFFER_SIZE);
 		buffer[read_ret] = '\0';
 		nl_pointer = strrchr(buffer, '\n');
@@ -67,26 +61,21 @@ lseek_ret = lseek(fd, -(1 + BUFFER_SIZE), SEEK_END);
 			linelen = strlen(nl_pointer);
 			write(1, nl_pointer, linelen);
 			while (buffers_without_nl) {
-				read(fd, remainder, remainder_len);
-				remainder[remainder_len] = '\0';
-				write(1, remainder, remainder_len);
-				write(1, "\n", 1);
-				lseek_ret = lseek(fd, -(BUFFER_SIZE + linelen + remainder_len + 1), SEEK_CUR);
-				remainder_len = 0;
 				read_ret = read(fd, buffer, BUFFER_SIZE);
+				write(1, buffer, BUFFER_SIZE);
 				buffer[read_ret] = '\0';
-				nl_pointer = strrchr(buffer, '\n');
+				buffers_was_printed = true;
 				buffers_without_nl--;
-				// continue ;
-				goto here;
 			}
-			write(1, "\n", 1);
+			if (!buffers_was_printed) {
+				write(1, "\n", 1);
+			}
 			buffer[nl_pos] = '\0';
 			lseek_ret = lseek(fd, -(linelen + 0), SEEK_CUR);
 			nl_pointer = strrchr(buffer, '\n');
+			buffers_was_printed = false;
 		}
-		remainder_len = strlen(buffer);
-		lseek_ret = lseek(fd, -(0 + remainder_len + BUFFER_SIZE), SEEK_CUR);
+		lseek_ret = lseek(fd, -(0 + 2 * BUFFER_SIZE), SEEK_CUR);
 	} while (lseek_ret != -1);
 	linelen = strlen(buffer);
 	write(1, buffer, linelen);
