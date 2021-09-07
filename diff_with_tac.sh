7b317673b0a1501c2fbd13d148b4d02038f44dc8
Safar Safarly <hafl@me.com>
2021-09-07 04:02:00 +0300

    testing script now is more readable

--- diff_with_tac.sh
+++ diff_with_tac.sh
@@ -1,17 +1,14 @@
-# test.txt
-
+# ############ diff with test.txt ############# #
 tac text.txt | cat -e > test_tac
 ./revert text.txt | cat -e > test_revert
-
 diff test_tac test_revert
 rm test_tac test_revert
+# ############################################# #
 
-# python random char generator
-
+# ###### diff with random char generator ###### #
 # python3 random_chars_generator.py > py.txt
-
 # tac py.txt | cat -e > test_tac
 # ./revert py.txt | cat -e > test_revert
-
 # diff test_tac test_revert
 # rm test_tac test_revert py.txt
+# ############################################# #
